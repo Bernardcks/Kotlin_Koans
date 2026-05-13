6f52073265f4416d22d53ca765be08a094c42f86
Bernard Chua <bernardchua.2024@computing.smu.edu.sg>
2026-05-13 12:38:42 +0800

    Properties(ALL): Basic properties, custom getters/setters, lazy access & lazy using delegate, custom delegates

--- Properties/Delegates how it works/src/Task.kt
+++ Properties/Delegates how it works/src/Task.kt
@@ -10,11 +10,12 @@ class EffectiveDate<R> : ReadWriteProperty<R, MyDate> {
     var timeInMillis: Long? = null
 
     override fun getValue(thisRef: R, property: KProperty<*>): MyDate {
-        TODO()
+        // timeInMillis!!.toDate() -> Use this instead, assert not null then convert to date
+        return timeInMillis?.toDate()!!
     }
 
     override fun setValue(thisRef: R, property: KProperty<*>, value: MyDate) {
-        TODO()
+        timeInMillis = value.toMillis()
     }
 }
 
